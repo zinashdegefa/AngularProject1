0ec82600c2ea0d694de2889d6625efe57ffdce3a
zinashdegefa <mydearbarkot@gmail.com>
2023-06-29 20:54:35 -0500

    angular project runs

--- AngularCrudWeb/src/app/app-routing.module.ts
+++ AngularCrudWeb/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PersonComponent } from './components/person/person.component';
+import { CarComponent } from './components/car/car.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'person', component: PersonComponent},
+  {path: 'car', component: CarComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AngularCrudWeb/src/app/app.module.ts
+++ AngularCrudWeb/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PersonComponent } from './components/person/person.component';
+import { CarComponent } from './components/car/car.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PersonComponent,
+    CarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
   ],
   providers: [],
   bootstrap: [AppComponent]
